496a8a6e572352f6492198ec46ea31eb323691cf
James McDaniel <icathaid@gmail.com>
2018-05-19 12:03:55 -0700

    put nefarious () back in

--- test/greet.spec.js
+++ test/greet.spec.js
@@ -5,7 +5,7 @@ const greet = require('../lib/greet');
 describe('greet module', () => {
     it('should return null if passed non string', () => {
         const expected = null;
-        const actual = greet;
+        const actual = greet();
         expect(actual).toBe(expected)
     })
 });
